c57159b785d5bf3424fcf5757ff2e9cc0ed1cfb6
k4584587 <k4584587@naver.com>
2021-04-23 01:07:19 +0900

    Swagger ui 설정 추가

---
+++ src/main/java/kr/needon/blog/config/swaggerConfig.java
@@ -0,0 +1,34 @@
+package kr.needon.blog.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class swaggerConfig {
+
+    @Bean
+    public Docket swaggerApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("kr.needon.blog.Controller"))
+                .paths(PathSelectors.ant("/v1/api/**"))
+                .build()
+                .apiInfo(swaggerInfo());
+    }
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder()
+                .title("Needon Blog API Docs")
+                .version("1")
+                .build();
+    }
+
+}
